e62d3d24f8a27a591f20aa343061f83759fdc144
princeadeayomide2019 <nurdade2019@gmail.com>
2020-03-31 23:39:20 +0100

    welcome togith

---
+++ script.js
@@ -0,0 +1,18 @@
+// var foo = 13;
+// var bar = '5';
+// console.log (foo * bar);// 65
+
+// var foo = "far";
+// var myNumber = Number(foo);
+// if (!isNaN(myNumber)){
+//     console.log("It's not a Number!");
+// }else console.log("It's a number");
+
+
+var x = 200.7;
+console.log(Math.round(x))// 201
+var a = 10, b = 50, c = 3;
+console.log(big = Math.max(a, b, c), small =
+    Math.min(a, b, c));
+console.log(Math.random(a, b, c));
+// Math.PI  Math.random()  .sqrt()  .log()
